fee55371491e462a05c51bbe219c4f70602c6f72
AtomicXZ <achubadyal4@gmail.com>
2021-01-05 16:31:29 +0530

    rosy: Add GcamGo

--- device.mk
+++ device.mk
@@ -131,7 +131,7 @@ PRODUCT_PACKAGES += \
     camera.msm8953 \
     libmm-qcamera \
     libui_shim \
-    Snap \
+    GCamGo \
     camera.device@3.2-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
